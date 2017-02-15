4a5694afd4dfde5f78fd7c355baadad5f1f01bb5
Gmes23 <gmesda23@gmail.com>
2017-02-15 00:05:57 -0500

    search bard works/ renders results as search item is typed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -7,28 +8,38 @@ import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAEEIfoMI7zfXkA74B15cGyOwve8U1_4vE';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+     this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+     YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+         videos: videos,
+         selectedVideo: videos[0]
+         });
     });
-
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 1000)
+
+
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+         onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+         videos={this.state.videos} />
       </div>
     );
   }
